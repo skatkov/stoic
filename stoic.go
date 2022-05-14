f3e1fa84187a4db10a287af7162fd5ec138bd836
Stanislav Katkov <contact@skatkov.com>
2022-05-14 13:35:01 +0200

    clean up

--- stoic.go
+++ stoic.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 )
 
 var editor = "/usr/bin/nano"
@@ -21,8 +20,6 @@ func main() {
 	openInEditor(directory)
 }
 
-//https://github.com/jotaen/klog/blob/7b5503c7e39a94f6bdf5213c43e41e8ec5422064/src/app/context.go#L352
-
 func openInEditor(filename string) error {
 	var editor = os.Getenv("EDITOR")
 	if editor == "" {
@@ -33,20 +30,4 @@ func openInEditor(filename string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	return cmd.Run()
-}
-
-func tryCommands(commands []string, additionalArgs string) bool {
-	for _, command := range commands {
-		args := strings.Split(command, " ")
-		args = append(args, additionalArgs)
-		cmd := exec.Command(args[0], args[1:]...)
-		cmd.Stdin = os.Stdin
-		cmd.Stdout = os.Stdout
-		err := cmd.Run()
-		if err == nil {
-			return true
-		}
-	}
-
-	return false
 }
