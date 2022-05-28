86c8b6f6b179bd5a5ed975f339ead29199128acc
Stanislav Katkov <contact@skatkov.com>
2022-05-29 01:46:45 +0200

    one more list

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module github.com/skatkov/stoic
 go 1.18
 
 require (
+	github.com/charmbracelet/bubbles v0.10.3
 	github.com/charmbracelet/bubbletea v0.20.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/stretchr/testify v1.7.1
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/charmbracelet/lipgloss v0.4.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -19,6 +22,7 @@ require (
 	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
--- stoic.go
+++ stoic.go
@@ -3,15 +3,30 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"time"
 
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
 	. "github.com/skatkov/stoic/src"
 )
 
 var BinaryVersion string   // Set via build flag
 var BinaryBuildHash string // Set via build flag
+
+const defaultWidth = 20
+const listHeight = 14
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+)
 
 func main() {
 	ctx := NewContext(
@@ -21,14 +36,35 @@ func main() {
 		os.Getenv("STOIC_TEMPLATE"),
 	)
 
-	list := flag.Bool("list", false, "list all entries")
-	about := flag.Bool("about", false, "display about info")
+	listFlag := flag.Bool("list", false, "list all entries")
+	aboutFlag := flag.Bool("about", false, "display about info")
 
 	flag.Parse()
 
-	if *list {
+	if *listFlag {
+		items := []list.Item{
+			item("Ramen"),
+			item("Tomato Soup"),
+			item("Hamburgers"),
+			item("Cheeseburgers"),
+			item("Currywurst"),
+			item("Okonomiyaki"),
+			item("Pasta"),
+			item("Fillet Mignon"),
+			item("Caviar"),
+			item("Just Wine"),
+		}
+
+		l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+		l.Title = "Which entry are you interested in?"
+		l.SetShowStatusBar(false)
+		l.SetFilteringEnabled(false)
+		l.Styles.Title = titleStyle
+		l.Styles.PaginationStyle = paginationStyle
+		l.Styles.HelpStyle = helpStyle
+
 		p := tea.NewProgram(entryList{
-			choices: []Entry{NewEntry(ctx, time.Now())},
+			list: l,
 		})
 		if err := p.Start(); err != nil {
 			fmt.Printf("Alas, there's been an error: %v", err)
@@ -38,7 +74,7 @@ func main() {
 		return // We're done here.
 	}
 
-	if *about {
+	if *aboutFlag {
 		fmt.Println(fmt.Sprintf("Version: %s", BinaryVersion) + "\n" + fmt.Sprintf("Build Hash: %s", BinaryBuildHash))
 		return // We're done here.
 	}
@@ -46,8 +82,37 @@ func main() {
 	_ = ctx.OpenInEditor(NewEntry(ctx, time.Now()))
 }
 
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                               { return 1 }
+func (d itemDelegate) Spacing() int                              { return 0 }
+func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s string) string {
+			return selectedItemStyle.Render("> " + s)
+		}
+	}
+
+	fmt.Fprintf(w, fn(str))
+}
+
 type entryList struct {
-	choices []Entry
+	list   list.Model
+	items  []item
+	choice string
 }
 
 func (m entryList) Init() tea.Cmd {
@@ -70,8 +135,5 @@ func (m entryList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m entryList) View() string {
-	s := "Listing entries...\n\n"
-
-	s += "\nPreq q to quit. \n"
-	return s
+	return "\n" + m.list.View()
 }
