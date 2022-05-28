6b57929a8d092776c2cdd79bb5098c57f650ef0c
Stanislav Katkov <contact@skatkov.com>
2022-05-29 00:49:12 +0200

    first implementation of list view

--- go.mod
+++ go.mod
@@ -3,12 +3,23 @@ module github.com/skatkov/stoic
 go 1.18
 
 require (
+	github.com/charmbracelet/bubbletea v0.20.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/stretchr/testify v1.7.1
 )
 
 require (
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/term v0.0.0-20210422114643-f5beecf764ed // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- stoic.go
+++ stoic.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	. "github.com/skatkov/stoic/src"
 )
 
@@ -26,14 +27,51 @@ func main() {
 	flag.Parse()
 
 	if *list {
-		fmt.Println("Listing entries...")
-		return
+		p := tea.NewProgram(entryList{
+			choices: []Entry{NewEntry(ctx, time.Now())},
+		})
+		if err := p.Start(); err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			os.Exit(1)
+		}
+
+		return // We're done here.
 	}
 
 	if *about {
 		fmt.Println(fmt.Sprintf("Version: %s", BinaryVersion) + "\n" + fmt.Sprintf("Build Hash: %s", BinaryBuildHash))
-		return
+		return // We're done here.
 	}
 
 	_ = ctx.OpenInEditor(NewEntry(ctx, time.Now()))
+}
+
+type entryList struct {
+	choices []Entry
+}
+
+func (m entryList) Init() tea.Cmd {
+	return nil
+}
+
+func (m entryList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Was ia a keypress?
+	case tea.KeyMsg:
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m entryList) View() string {
+	s := "Listing entries...\n\n"
+
+	s += "\nPreq q to quit. \n"
+	return s
 }
