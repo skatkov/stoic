b59eace18bef23d7389cce3e8f9209b67c9bd1f0
Stanislav Katkov <contact@skatkov.com>
2022-06-13 02:44:49 +0200

    fixing specs

--- cmd/edit_test.go
+++ cmd/edit_test.go
@@ -11,6 +11,7 @@ import (
 func TestNewEditCommand(t *testing.T) {
 	ctx := s.NewContext("", "", "", "")
 	command := NewEditCommand(ctx, "yesterday")
+	y, m, d := time.Now().AddDate(0, 0, -1).Date()
 
-	assert.Equal(t, time.Now().AddDate(0, 0, -1), command.Date())
+	assert.Equal(t, time.Date(y, m, d, 0, 0, 0, 0, time.Now().Location()), command.Date())
 }
